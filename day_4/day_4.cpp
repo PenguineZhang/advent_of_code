e5e5fbee396f4e45d3c66404e22c83a8d50b888c
Peng Zhang <pzhang@spawar.navy.mil>
2018-12-17 17:32:17 -0800

    starting day_4

--- day_4/day_4.cpp
+++ day_4/day_4.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
 
+using namespace std;
+
+void strip(string src, string delimiter, vector<string>& output){
+	
+	output.push_back(src.substr(0, src.find(delimiter)));
+
+}
 
 int main(int argc, char** argv){
 	if(argc != 2){
@@ -7,4 +18,10 @@ int main(int argc, char** argv){
 		return -1;
 	}
 
+	ifstream f(argv[1], ios::in);
+	string line;
+	while(getline(f, line)){
+		
+
+	}
 }
